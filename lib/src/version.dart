b9a251b267313883ad8c65151bd7ec3ca625fec5
Uladzimir Klyshevich <istin2007@gmail.com>
2026-09-10 00:34:14 +0300

    chore(release): v0.1.5 (#37)

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -5,7 +5,7 @@
 library;
 
 /// Current package version, mirroring `pubspec.yaml`.
-const String dmtoolsVersion = '0.1.4';
+const String dmtoolsVersion = '0.1.5';
 
 /// Returns the version line printed by `dmtools --version`.
 String versionLine() => 'dmtools $dmtoolsVersion';

70877de3630ab36be264ba5f9220334240168d39
AI Teammate <agent.ai.native@gmail.com>
2026-09-22 06:42:44 +0000

    gh-192 WIP auto-save 2026-09-22T06-42-44

--- test/agents/cli_agent_test.dart
+++ test/agents/cli_agent_test.dart
@@ -16,6 +16,7 @@ void main() {
   filterEnvTests();
   executeCommandsTests();
   readOutputResponseTests();
+  failureResponseTests();
   lifecycleOrderTests();
   lifecycleInputCleanupTests();
   lifecycleFolderCleanupTests();
